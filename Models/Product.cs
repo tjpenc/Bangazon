d05082838498e854ec2f73243565230844938626
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-08-22 19:12:30 -0400

    Setup of entity framework and created classes

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string SellerId { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Quantity { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BangazonAPI.Models
+{
+    public class User
+    {
+        public int Uid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsSeller { get; set; }
+    }
+}
